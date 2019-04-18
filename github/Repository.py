602331ebf74382f76b1c2827dd769c272eadf447
Lionel Vital <lionel@tenxer.com>
2019-04-18 10:20:50 -0700

    Implement create_check_run method in repository
    Problem:
    
    Solution:
    
    Test plan:
    
    Task:

--- github/Repository.py
+++ github/Repository.py
@@ -829,6 +829,53 @@ class Repository(github.GithubObject.CompletableGithubObject):
         )
         return github.Comparison.Comparison(self._requester, headers, data, completed=True)
 
+    def create_check_run(self, name, head_sha, details_url=github.GithubObject.NotSet, external_id=github.GithubObject.NotSet, status=github.GithubObject.NotSet, started_at=github.GithubObject.NotSet, conclusion=github.GithubObject.NotSet, completed_at=github.GithubObject.NotSet, output=github.GithubObject.NotSet, actions=github.GithubObject.NotSet):
+        """
+        :calls: `POST /repos/:owner/:repo/check-run <http://developer.github.com/v3/checks/runs>`_
+        :param name: string
+        :param head_sha: string
+        :param details_url: string
+        :param external_id: string
+        :param status: string
+        :param started_at: string
+        :param conclusion: string
+        :param completed_at: string
+        :param output: dict
+        :param actions: list of dict
+        :rtype: :class:`github.CheckRun.CheckRun`
+        """
+        assert isinstance(name, (str, unicode)), name
+        assert isinstance(head_sha, (str, unicode)), head_sha
+        assert isinstance(details_url, (str, unicode)), details_url
+        assert isinstance(external_id, (str, unicode)), external_id
+        assert isinstance(status, (str, unicode)), status
+        assert isinstance(started_at, (str, unicode)), started_at
+        assert isinstance(conclusion, (str, unicode)), conclusion
+        assert isinstance(completed_at, (str, unicode)), completed_at
+        # TODO: create new classes for output and actions?
+        assert isinstance(output, dict), output
+        assert all(isinstance(element, dict) for element in actions), actions
+        post_parameters = {
+            "name": name,
+            "head_sha": head_sha,
+            "details_url": details_url,
+            "external_id": external_id,
+            "status": status,
+            "started_at": started_at,
+            "conclusion": conclusion,
+            "completed_at": completed_at,
+            "output": output,
+            "actions": actions,
+        }
+        headers, data = self._requester.requestJsonAndCheck(
+            "POST",
+            self.url + "/check-runs",
+            input=post_parameters
+        )
+        return data
+        # TODO: create a class for check runs and return an object
+        # return github.GitCommit.GitCommit(self._requester, headers, data, completed=True)
+
     def create_git_blob(self, content, encoding):
         """
         :calls: `POST /repos/:owner/:repo/git/blobs <http://developer.github.com/v3/git/blobs>`_
